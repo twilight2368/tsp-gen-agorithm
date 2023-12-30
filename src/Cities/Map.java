5c8c8fe85da998ff5367a3dede9a4ceed422518f
Anonymous <103519498+twilight2368@users.noreply.github.com>
2023-12-30 20:11:50 +0700

    now

--- src/Cities/Map.java
+++ src/Cities/Map.java
@@ -1,24 +1,22 @@
 package Cities;
 
 public class Map {
-    private int number_of_cities;
-    private int[][] my_map;
-
+    public static int number_of_city;
+    private static int[][] my_map;
 
     public Map(int number_of_cities, int[][] map) {
-        this.number_of_cities = number_of_cities;
-        for(int i = 0; i < number_of_cities; i++){
-            for(int j = 0; j < number_of_cities; j++){
+        number_of_city = number_of_cities;
+
+        for (int i = 0; i < number_of_cities; i++) {
+            for (int j = 0; j < number_of_cities; j++) {
                 my_map[i][j] = map[i][j];
             }
         }
     }
 
-    public int getNumberofCities(){
-        return this.number_of_cities;
-    }
+    
 
-    public int getDistance(int a, int b){
+    public static int getDistance(int a, int b) {
         return my_map[a][b];
     }
 }
--- src/Population/Chromosome.java
+++ src/Population/Chromosome.java
@@ -5,13 +5,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+import Cities.Map;
+
 public class Chromosome {
     private List<Integer> gene = new ArrayList<Integer>();
     public static int number_of_gen;
 
     public Chromosome(int number_of_cities) {
         number_of_gen = number_of_cities;
-        for (int i = 1; i <= number_of_cities; i++) {
+
+        for (int i = 1; i <= number_of_gen; i++) {
             gene.add(i);
         }
         Collections.shuffle(gene);
@@ -25,16 +28,16 @@ public class Chromosome {
         return this.gene;
     }
 
-    public void addGene(Integer add_gene){
+    public void addGene(Integer add_gene) {
         gene.add(add_gene);
     }
 
-    public Chromosome mutation(Chromosome pair_mutantion) {
+    public Chromosome crossOver(Chromosome pair_mutantion) {
         Chromosome child = new Chromosome();
         Random random = new Random();
 
         for (int i = 0; i < random.nextInt(number_of_gen); i++) {
-                child.addGene(pair_mutantion.getGene().get(i));
+            child.addGene(pair_mutantion.getGene().get(i));
         }
 
         for (int i = 0; i < number_of_gen; i++) {
@@ -46,6 +49,36 @@ public class Chromosome {
         return child;
     }
 
-    
+    public Chromosome mutation() {
+        Chromosome child = new Chromosome();
+        Random random = new Random();
 
+        int mutation_point = random.nextInt(1, number_of_gen - 1);
+
+        for (int i = 0; i < number_of_gen; i++) {
+            if (i == mutation_point - 1) {
+                child.addGene(gene.get(mutation_point + 1));
+            } else if (i == mutation_point + 1) {
+                child.addGene(gene.get(mutation_point - 1));
+            } else {
+                child.addGene(gene.get(i));
+            }
+        }
+
+        return child;
+    }
+
+    public int getValue(){
+        int sum = 0;
+        sum = sum + Map.getDistance(0, gene.get(0));
+
+        for (int i = 0; i < number_of_gen - 1; i++) {
+            sum = sum + Map.getDistance(gene.get(i), gene.get(i + 1));
+        }
+
+        sum = sum + Map.getDistance(gene.get(number_of_gen - 1), 0);
+        
+        return sum;
+    }
+    
 }
